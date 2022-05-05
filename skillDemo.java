4f0aeb9d38d8740f7c0fec58fd9e720fc861e6f9
Pathe-Seck <pathemseck1502@gmail.com>
2022-05-04 22:20:43 -0700

    skillDemo updatee

--- skillDemo.java
+++ skillDemo.java
@@ -1,5 +1,13 @@
 public class skillDemo{
 
+    public static int sum(int[]a){
+        int summed = 0;
+        for (int i = 0; i < a.length; i++){
+            summed = summed + i;
+        }
+        return summed;
+    }
+
 
 
 }
---
+++ skillDemoTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemoTest {
+
+
+    int [] trial = {1,2,3,4,5};
+    
+    @Test
+    public void summed(){
+        assertEquals(20, skillDemo.sum(trial));
+    }
+
+    
+}
